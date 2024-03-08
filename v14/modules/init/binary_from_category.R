71779c5e91b7fb4d6bbd650921a042110786e538
V-Dem Institute <contact@v-dem.net>
2024-03-08 16:49:07 +0100

    v14 update

---
+++ v14/modules/init/binary_from_category.R
@@ -0,0 +1,109 @@
+#!/usr/bin/env Rscript
+
+# ------------------------------------------------------------------------------
+# Create binary variables from baseline categories
+# For this module to be successful, we also need to remove the category that we are performing the binary transform on. Note that 'binary transform' means to transform the baseline category into a 0 and 1 for all other cases
+# Check the codebook to find out which category is the 'baseline' category in the context of the transform.
+# ------------------------------------------------------------------------------
+
+suppressMessages(library(dplyr))
+suppressMessages(library(vutils))
+suppressMessages(library(vpipe))
+
+#
+# Functions
+# --------------------------------------------------------------------------
+# Merge in qtable
+merge_qtable <- function(df, qtable) {
+
+    inRow <- nrow(df)
+    df <- merge(
+        x = df,
+        y = qtable[, c("question_id", "name", "recode_category_to_binary")],
+        by = "question_id",
+        all.x = TRUE)
+    stopifnot(inRow == nrow(df))
+    stopifnot(!all(is.na(df[["recode_category_to_binary"]])))
+
+    return(df)
+}
+
+# turn baseline to 0 all others 1 and transform question_id
+binary_version <- function(df, qtable) {
+
+    df[["code"]] <-
+        ifelse(
+            test = df[["code"]] == df[["recode_category_to_binary"]],
+            yes = 0, no = 1)
+    df[["name"]] <- paste0(df[["name"]], "bin")
+    df[["question_id"]] <-
+        trans(df[["name"]], to = "question_id", ttable = qtable, by = "name")
+
+    stopifnot(all(df[["code"]] %in% c(0, 1, NA_real_)))
+    return(df)
+}
+
+# transform original version: remove the baseline category
+original_cleanup <- function(df) {
+
+    recCat <- unique(df[["recode_category_to_binary"]])
+    codeTable <- table(df[["code"]], useNA = "always")
+    expectedToBeRecoded <- codeTable[names(codeTable)==recCat & !is.na(names(codeTable))]
+    
+    # For these variables we need to adjust the categories by -1
+    originals <- c("v2mecenefi", "v2lgdsadlo")
+
+    # Drop ratings where code == recode_category_to_binary
+    dropID <- df[["id"]][with(df, code == recode_category_to_binary & !is.na(code))]
+    stopifnot(length(dropID) == expectedToBeRecoded)
+    df <- df[!df[["id"]] %in% dropID, ]
+    info(sprintf("Dropping %s observations following binary recoding",
+        length(dropID)))
+
+    # Shift by -1 for originals
+    df[["code"]] <- ifelse(df[["name"]] %in% originals,
+        df[["code"]] - 1, df[["code"]])
+    
+    # Code can be missing, but ID values should not be missing
+    with(df, {
+        stopifnot(!anyNA(country_id))
+        stopifnot(!anyNA(question_id))
+        stopifnot(!anyNA(coder_id))
+        stopifnot(!anyNA(historical_date))
+    })
+
+    inNA <- codeTable[is.na(names(codeTable))]
+    stopifnot(inNA == sum(is.na(df[["code"]])))
+    df[["recode_category_to_binary"]] <- NULL
+    return(df)
+}
+
+main <- function(qtable, df) {
+    
+    df <- merge_qtable(df, qtable)
+    out <- list(original_cleanup(df), binary_version(df, qtable))
+    return(out)
+
+}
+
+# Run script
+if (no_test()) {
+    # Global variables
+    db <- pg_connect()
+    get_globals()
+
+    # Imports
+    qtable <-  load_qtable()
+    objs <- find_dep_files(TASK_ID, db)
+    df <- objs[[TASK_NAME]][[TASK_NAME]]
+
+    # Run
+    collectedInputs <- named_list(qtable, df)
+    setNames(do.call(main, collectedInputs), 
+        c(TASK_NAME, TASK_NAME %^% "bin")) %>%
+        write_file(., OUTFILE, dir_create = T)    
+    info("Done with binary_from_category...")
+} else {
+    testthat::test_file("~/proj/vdemds/module_unit_tests/init/test_binary_from_category.R") %>%
+		check_test_file_output()
+}
---
+++ v14/modules/init/clean_by_utable.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+# ------------------------------------------------------------------------------
+# This script performs an inner join between CD/CY and utable using
+# country_id and year as keys.
+# ------------------------------------------------------------------------------
+
+suppressMessages(library(dplyr))
+suppressMessages(library(vutils))
+suppressMessages(library(vpipe))
+
+#
+# Functions
+# --------------------------------------------------------------------------
+
+clean_by_utable <- function(df, utable) {
+
+	YEAR_ADDED <- FALSE
+	if (!"year" %in% names(df)) {
+		df$year <- to_year(df$historical_date)
+		YEAR_ADDED <- TRUE
+	}
+	stopifnot(`country_text_id column is missing!` = 
+		"country_id" %in% names(df))
+    
+    # inner join
+    outdf <- merge(df, utable[, c("country_id", "year")],
+        by = c("country_id", "year"))
+    
+    # Define observations that are not in outdf but in df
+    dfFactor <- interaction(df[, c("country_id", "year")], drop = FALSE)
+    outdfFactor <- interaction(outdf[, c("country_id", "year")], drop = FALSE)
+    removedObservations <- organiseRows(df[!dfFactor %in% outdfFactor,], country_id, year)
+
+    DIRTYLIST[["clean_by_utable"]][[deparse(substitute(df))]] <<- removedObservations
+
+	if (YEAR_ADDED) {
+		outdf$year <- NULL
+	}
+
+	return(outdf)
+}
+
+main <- function(utable, cd, cy) {
+	df_cd <- organiseRows(clean_by_utable(cd, utable), country_id, historical_date)
+    df_cy <- organiseRows(clean_by_utable(cy, utable), country_id, year)
+    return(list(cd = df_cd, cy = df_cy))
+}
+
+# Run script
+if (no_test()) {
+    # Global variables
+    db <- pg_connect()
+    get_globals()
+
+    # Imports
+	utable <- load_country_unit()[, c("country_id", "year")]
+	objs <- find_dep_files(TASK_ID, db)
+    cd <- objs[[TASK_NAME]][[TASK_NAME]]$cd
+	cy <- objs[[TASK_NAME]][[TASK_NAME]]$cy
+
+    # Run
+    collectedInputs <- named_list(utable, cd, cy)
+    setNames(list(do.call(main, collectedInputs), DIRTYLIST), 
+        c(TASK_NAME, "dirt")) %>% 
+        write_file(., OUTFILE, dir_create = TRUE)    
+} else {
+    testthat::test_file("~/proj/vdemds/module_unit_tests/init/test_clean_by_utable.R") %>%
+		check_test_file_output()
+}
