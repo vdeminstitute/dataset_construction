4dbaafba3d519cd559e9a167a5b51636dd41e6c2
V-Dem Institute <contact@v-dem.net>
2024-03-08 17:03:31 +0100

    v14 update

---
+++ v14/modules/downstream_cleaning/elbalpap_cleaning.R
@@ -0,0 +1,121 @@
+#!/usr/bin/env Rscript
+
+# ==========================================================================
+# C-variable v3elbalstat gets cleaned by C-variable v3elbalpap per coder!
+# ==========================================================================
+
+suppressMessages(library(dplyr))
+suppressMessages(library(vutils))
+suppressMessages(library(vpipe))
+
+#
+# Functions
+# --------------------------------------------------------------------------
+
+# Check cleaning rule
+check_cleaning_rule <- function(codebook, cleaningVar) {
+    cleaning <- codebook[["cleaning"]]
+    stopifnot(`rule from codebook is of length zero` = length(cleaning) > 0)
+    
+    ruleVec <- unlist(strsplit(x = cleaning, split = ";"))
+    rule <- ruleVec[grepl(cleaningVar, ruleVec)]
+    stopifnot(`after splitting the rule, found some delimiters` =
+        !grepl(pattern = "[;:/]", x = rule))
+
+    stopifnot(`no cleaning rule could be found` = length(rule) == 1)
+    stopifnot(`we only set values to missing`= grepl("missing", rule))
+    stopifnot(`we only remove based on numeric values for cleaning_var` = 
+        grepl("\\d\\.?$", rule))
+    stopifnot(!is.na(rule))
+    stopifnot(rule != "")
+	stopifnot(grepl(cleaningVar, rule, fixed = TRUE))
+
+    return(rule)
+}
+
+# Merge in cleaningVarDf
+merge_elbalpap <- function(df, cleaningVarDf, utable) {
+
+    stopifnot(`elbalbap_df is not a data.frame` = is.data.frame(cleaningVarDf))
+    stopifnot(`df is not a data.frame` = is.data.frame(df))
+
+    cleaningVarDf[["cleaningVar"]] <- cleaningVarDf[["code"]]
+    cleaningVarDf <- cleaningVarDf[, c("cleaningVar", "country_id", "historical_date", "coder_id")]
+
+    stopifnot("code" %in% names(df))
+    stopifnot(!"code" %in% names(cleaningVarDf))
+
+    df <-
+        merge(x = df, y = cleaningVarDf, 
+            by = c("country_id", "historical_date", "coder_id"), 
+            all= TRUE) |>
+        vutils::interpolate_components(
+            df=_, cols = "cleaningVar", utable,
+            keep_nan = FALSE, coder_level = TRUE) |>
+        organiseRows(df=_, country_id, historical_date)
+    df <- df[with(df, !is.na(code) | !is.na(text_answer)), , drop = FALSE]
+    stopifnot("cleaningVar" %in% names(df))
+
+    return(df)
+}
+
+set_to_missing_elbalpap <- function(df) {
+
+    # if elbalpap = 0 only, set to missing
+    bool <- df[["cleaningVar"]] == 0 & !is.na(df[["cleaningVar"]])
+    stopifnot(`bool cannot have missingness` = !anyNA(bool))
+
+    if (any(bool)) {
+        df <- clean_observations(df, bool, "set_to_missing_elbalpap",
+            "Observations set to missing based on elbalpap = 0")
+    }
+
+    df[["cleaningVar"]] <- NULL
+    return(df)
+}
+
+main <- function(utable, codebook, df, cleaningVarDf, cleaningVar, TASK_NAME) {
+    
+    # Assert and retrieve cleaning rule
+    rule <- check_cleaning_rule(codebook, cleaningVar)
+    info(sprintf("Running %s_cleaning for %s with rule: [%s]",
+        cleaningVar, TASK_NAME, rule))
+    df <-
+        merge_elbalpap(df, cleaningVarDf, utable) |> 
+        set_to_missing_elbalpap(df=_) |> 
+        vbase::organiseRows(df=_, country_id, historical_date)
+
+    return(df)
+}
+
+# Run script
+if (no_test()) {
+    # Global variables
+    db <- pg_connect()
+    get_globals()
+
+    # Imports
+    utable <- load_country_unit()
+    codebook <- load_codebook()
+    qtable <- load_qtable()
+    codebook <- codebook[codebook$tag == TASK_NAME, ]
+    qtable <- qtable[qtable$name == TASK_NAME, ]
+    objs <- find_dep_files(TASK_ID, db)
+    df <- objs[[TASK_NAME]][[TASK_NAME]]
+    cleaningVar <- ifelse(grepl("^v2", TASK_NAME), "v2elbalpap", "v3elbalpap")
+
+    if (grepl("^v2", cleaningVar)) {
+        cleaningVarDf <- objs[["v2elbalpap"]][["v2elbalpap"]]
+    } else {
+        cleaningVarDf <- objs[["v3elbalpap"]][["v3elbalpap"]]
+    }
+
+    # Run
+    collectedInputs <- named_list(utable, codebook, df, cleaningVarDf, cleaningVar, TASK_NAME)
+    setNames(list(do.call(main, collectedInputs), DIRTYLIST), 
+        c(TASK_NAME, "dirt")) %>%
+        write_file(., OUTFILE, dir_create = TRUE)    
+    info("Done with elbalpap_cleaning")
+} else {
+    testthat::test_file("~/proj/vdemds/module_unit_tests/downstream_cleaning/test_elbalpap_cleaning.R")
+}
---
+++ v14/modules/downstream_cleaning/hog_appointment_dates.R
@@ -0,0 +1,99 @@
+#!/usr/bin/env Rscript
+
+# ------------------------------------------------------------------------------
+# cleaned by a-star v2exnamhog and v3exnamhog
+# This script is hardcoded to set code to missing if exnamhog is missing.
+# ------------------------------------------------------------------------------
+
+suppressMessages(library(dplyr))
+suppressMessages(library(vutils))
+suppressMessages(library(vpipe))
+
+#
+# Functions
+# --------------------------------------------------------------------------
+check_date_specific_rule <- function(codebook, TASK_NAME) {
+
+    stopifnot(`TASK_NAME needs to be of class character` = is.character(TASK_NAME))
+    stopifnot(`TASK_NAME needs to be of length 1` = length(TASK_NAME) == 1)
+    stopifnot(`TASK_NAME cannot be an empty character vector` = TASK_NAME != "")
+
+    rule <- codebook[["date_specific"]]
+    stopifnot(length(rule) == 1)
+    stopifnot(!is.na(rule))
+    stopifnot(rule != "")
+    stopifnot(grepl("HOG appointment dates", fixed = TRUE, x = rule))
+}
+
+merge_exnamhog <- function(df, exnamhog) {
+    
+    stopifnot("code" %in% names(df))
+
+    exnamhog <- exnamhog[, c("country_id", "historical_date", "text_answer")]
+    names(exnamhog) <- c("country_id", "historical_date", "exnamhog")
+    stopifnot(!"text_answer" %in% names(exnamhog))
+    
+    df <- merge(
+        x = df,
+        y = exnamhog,
+        by = c("country_id", "historical_date"),
+        all.x = TRUE)
+    stopifnot("exnamhog" %in% names(df))
+
+    return(df)
+}
+
+# Set the value of df to missing where exnamhog is missing
+set_to_missing <- function(df) {
+    
+    bool <- is.na(df[["exnamhog"]])
+    stopifnot(`bool cannot have missingness` = !anyNA(bool))
+
+    if (any(bool)) {
+
+        df <- clean_observations(df, bool, "set_to_missing_exnamhog",
+            "Observations set to missing where exnamhog is missing")
+    }
+
+    df[["exnamhog"]] <- NULL
+    
+    return(df)
+}
+
+main <- function(codebook, df, exnamhog, TASK_NAME) {
+    
+    check_date_specific_rule(codebook, TASK_NAME)
+    out <- 
+        merge_exnamhog(df, exnamhog) |>  
+		set_to_missing(df=_)
+
+    return(out)
+}
+
+
+# Run script
+if (no_test()) {
+    # Global variables
+    db <- pg_connect()
+    get_globals()
+
+    # Imports
+    codebook <- load_codebook()
+    codebook <- codebook[codebook$tag == TASK_NAME, ]
+    objs <- find_dep_files(TASK_ID, db)
+    df <- objs[[TASK_NAME]][[TASK_NAME]]
+    if (grepl("^v2", TASK_NAME)) {
+        exnamhog <- objs[["v2exnamhog"]][["v2exnamhog"]]
+    } else {
+        exnamhog <- objs[["v3exnamhog"]][["v3exnamhog"]]
+    }
+
+    # Run
+    collectedInputs <- named_list(codebook, df, exnamhog, TASK_NAME)
+    setNames(list(do.call(main, collectedInputs), DIRTYLIST), 
+        c(TASK_NAME, "dirt")) %>%
+        write_file(., OUTFILE, dir_create = TRUE)    
+    info("Done with hog appointment date cleaning")
+} else {
+    testthat::test_file("~/proj/vdemds/module_unit_tests/downstream_cleaning/test_hog_appointment_dates.R")
+}
