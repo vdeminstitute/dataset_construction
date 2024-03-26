616756157319427faf695946b8ffba466174af5e
V-Dem Institute <contact@v-dem.net>
2024-03-26 14:34:49 +0100

    v14 update

--- vutils/src/RcppExports.cpp
+++ vutils/src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // by_year
 List by_year(DataFrame x, DateVector dates, Function fn);
 RcppExport SEXP _vutils_by_year(SEXP xSEXP, SEXP datesSEXP, SEXP fnSEXP) {
